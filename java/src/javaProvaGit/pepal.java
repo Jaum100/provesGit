9d7494da295b51381177b7780d0ef369eaa3dca4
unknown <jaumedosmil@gmail.com>
2024-12-11 20:50:13 +0100

    versio

---
+++ java/src/javaProvaGit/pepal.java
@@ -0,0 +1,11 @@
+package javaProvaGit;
+
+public class pepal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hola mon");
+		System.out.println("Adeu mon");
+	}
+
+}
